fbd17d96d749dd8a9eebb895bf17d9cb0dd2dc49
zhangming <zhangming@weidian.com>
2020-04-09 18:19:28 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -6,7 +6,7 @@ describe('webpack.base.js test case', () => {
     console.log(baseConfig);
 
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, '/Users/zhangming/my-project/builder-webpack/test/smoke/template/src/index/index.js');
-        assert.equal(baseConfig.entry.search, '/Users/zhangming/my-project/builder-webpack/test/smoke/template/src/search/index.js')
+        assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js') > -1, true);
+        assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js') > -1, true)
     })
 });
